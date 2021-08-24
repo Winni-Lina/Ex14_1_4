0e95b5b55dd46c05bcccf31de329625f5a407945
Winni_Lina <9811011sa01@gmail.com>
2021-08-24 20:42:33 +0900

    Completion Ex14_1_4

--- app/src/main/java/kr/hs/emirim/w2026/ex14_1_4/MusicService.java
+++ app/src/main/java/kr/hs/emirim/w2026/ex14_1_4/MusicService.java
@@ -2,13 +2,19 @@ package kr.hs.emirim.w2026.ex14_1_4;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.IBinder;
+
+import android.app.Service;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.IBinder;
 import android.util.Log;
+import android.widget.Toast;
 
-public class MysicService extends Service {
+public class MusicService extends Service {
     MediaPlayer mp;
-    public MysicService() {
+
+    public MusicService() {
     }
 
     @Override
@@ -18,24 +24,26 @@ public class MysicService extends Service {
     }
 
     @Override
-    public void onCreate() {
-        Log.i("서비스 테스트:","onCreate() 호출됨");
+    public void onCreate(){
+        Log.i("서비스 테스트::", "onCreate(); 호출됨");
         super.onCreate();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.i("서비스 테스트:","onStartCommand() 호출됨");
+        Log.i("서비스 테스트::", "onStartCommand(); 호출됨");
+        Toast.makeText(getApplication(), "onStartCommand() 호출됨", Toast.LENGTH_LONG).show();
         mp = MediaPlayer.create(this, R.raw.woman);
-        mp.setLooping(true);
+//        mp.setLooping(true);
         mp.start();
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onDestroy() {
-        Log.i("서비스 테스트:","onDestroy() 호출됨");
+        super.onDestroy();
+        Log.i("서비스 테스트::", "onDestroy(); 호출됨");
+        mp.stop();
         super.onDestroy();
     }
-
 }
